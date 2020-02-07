a55a56278c21a0fd89f1496f31051b7adaf002ae
Thomas Maisel <thomai@posteo.de>
2020-02-07 13:27:48 +0100

    Implemented crop action

---
+++ ActionsModule/Actions/CropAction.cs
@@ -0,0 +1,82 @@
+﻿using ActionsModule.Attributes;
+using OpenCvSharp;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ActionsModule.Actions
+{
+    public class CropAction : ImageAction
+    {
+        public CropAction()
+        {
+            this.Name = "Crop";
+            this.Action = (input) =>
+            {
+                try
+                {
+                    var roi = new Rect((int)this.X, (int)this.Y, (int)this.Width, (int)this.Height);
+                    var result = new Mat(input, roi);
+                    input.Dispose();
+                    this.HasError = false;
+                    return result;
+                }
+                catch (Exception e)
+                {
+                    HasError = true;
+                    this.ErrorMessage = e.Message;
+                }
+
+                return input;
+            };
+        }
+
+        private double x = 0;
+        [Slider(0, 3000)]
+        public double X
+        {
+            get { return this.x; }
+            set
+            {
+                SetProperty(ref this.x, value);
+            }
+        }
+        
+        private double y = 0;
+        [Slider(0, 3000)]
+        public double Y
+        {
+            get { return this.y; }
+            set
+            {
+                SetProperty(ref this.y, value);
+            }
+        }
+        
+        private double width = 100;
+        [Slider(0, 3000)]
+        public double Width 
+        {
+            get { return this.width; }
+            set
+            {
+                SetProperty(ref this.width, value);
+            }
+        }
+        
+        private double height = 100;
+        [Slider(0, 3000)]
+        public double Height 
+        {
+            get { return this.height; }
+            set
+            {
+                SetProperty(ref this.height, value);
+            }
+        }
+    }
+}
